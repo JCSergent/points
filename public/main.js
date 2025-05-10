3460ff4acf550a6a29f55e9f02fd02b778e9c734
JCSergent <jcsergent20@gmail.com>
2025-05-10 18:07:37 -0400

    big updates

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+import { Modal } from "bootstrap";
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:8524");
@@ -9,7 +10,15 @@ if (playerId == null) {
     sessionStorage.setItem('x-unique-id', playerId);
 }
 
-// Load RoomId
+// Load PlayerName
+var playerName = sessionStorage.getItem('x-name');
+console.log(playerName);
+if (playerName == null) {
+    playerName = '???';
+    sessionStorage.setItem('x-name', playerName);
+}
+
+// Load Room from Path
 var pathParam = location.pathname.split("/").at(-1);
 if (pathParam.length == 4) {
     document.getElementById("room-code").value = pathParam;
@@ -20,7 +29,7 @@ if (pathParam.length == 4) {
 // Landing Page
 //
 document.getElementById("create-room").addEventListener("click", () => {
-    socket.emit('ROOM_CREATE_REQUEST', { playerId: playerId }, (res) => joinRoom(res));
+    socket.emit('ROOM_CREATE_REQUEST', { playerId: playerId, playerName: playerName }, (res) => joinRoom(res));
 });
 
 document.getElementById("join-room").addEventListener("click", () => {
@@ -29,7 +38,7 @@ document.getElementById("join-room").addEventListener("click", () => {
 });
 
 function connectRoom(code) {
-    socket.emit('ROOM_JOIN_REQUEST', { roomCode: code, playerId: playerId }, (res) => {
+    socket.emit('ROOM_JOIN_REQUEST', { roomCode: code, playerId: playerId, playerName: playerName }, (res) => {
         if (res != null) {
             joinRoom(res);
         } else {
@@ -56,46 +65,85 @@ function joinRoom(room) {
 
 var roomId = '';
 
-function renderRoom(roomState) {
-    console.log('rendering room');
-
-    document.getElementById("title").textContent = 'Room ' + roomState.id;
-
-    const playerElem = document.getElementById('players');
+function renderRoom(room) {
+    document.getElementById("title").textContent = 'Room ' + room.id;
 
     let playerNodes = '';
-    for (const playerEntry of Object.entries(roomState.players)) {
-        playerNodes += createPlayerNode(playerEntry[0], playerEntry[1]);
+    let scores = new Map();
+    for (const playerEntry of Object.entries(room.players)) {
+        playerNodes += createPlayerNode(playerEntry[0], playerEntry[1], room.state);
+        if (playerEntry[1].card != '') {
+            const tally = scores.get(playerEntry[1].card) ?? 0;
+            scores.set(playerEntry[1].card, tally + 1);
+        }
     }
-    playerElem.innerHTML = playerNodes;
+    document.getElementById('players').innerHTML = playerNodes;
 
-    const playersCard = roomState.players[playerId].card;
+    const playersCard = room.players[playerId].card;
     for (const card of cards) {
         card.classList.remove('bg-primary');
         if (card.textContent == playersCard) {
             card.classList.add('bg-primary');
         }
     }
+
+    document.getElementById('reveal').classList.remove('d-none');
+    document.getElementById('reset').classList.remove('d-none');
+    document.getElementById('stats').classList.remove('d-none');
+    if (room.state == 'reveal') {
+        document.getElementById('reveal').classList.add('d-none');
+
+        let scoreNodes = '';
+        const totalTally = scores.values().reduce((a,b) => a + b);
+        for (var score of scores.entries()) {
+            scoreNodes += createScoreNode(score[0], score[1], totalTally);
+        }
+        document.getElementById('stats').innerHTML = scoreNodes;
+    } else {
+        document.getElementById('reset').classList.add('d-none');
+        document.getElementById('stats').classList.add('d-none');
+    }
 };
 
 
-function createPlayerNode(id, player) {
+function createPlayerNode(id, player, gameState) {
     const isPlayer = id == playerId;
-    const hasSelected = player.card != null;
+    const hasSelected = player.card != '';
+    const isHidden = gameState == 'hidden';
+
     return `<div class="col text-center">
         <div class="d-flex flex-column justify-content-center">
             <div class="d-flex justify-content-center">
                 <p class="border border-primary border-3 rounded p-3 ${hasSelected ? 'bg-primary' : ''}">
-                    ?
+                    ${isHidden ? '?' : player.card == '' ? '-' : player.card }
                 </p>
             </div>
             <i class="fa-solid fa-user fs-1 text-light"></i>
             <div class="mt-2">
                 ${player.name}
-                ${isPlayer ?  '<button id="edit-name" class="btn btn-outline-light btn-sm"><i class="fa-solid fa-pen-to-square"></i></button>' : ''}
+                ${isPlayer ?  '<button id="edit-name" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-outline-light btn-sm"><i class="fa-solid fa-pen-to-square"></i></button>' : ''}
             </div>
         </div>
     </div>`;
+}
+
+function createScoreNode(card, tally, totalTally) {
+    return `
+    <div class="bar-wrapper">
+      <div class="bar text-bg-success" style="height: ${(tally / totalTally) * 100}%;">
+        ${tally}
+        </div>
+      <div class="bar-label">${card}</div>
+    </div>
+`;
+    // return `<div class="mb-3">
+    //         <label class="fs-2">${card}</label>
+    //         <div class="progress" style="height: 24px">
+    //             <div class="progress-bar text-bg-success" style="width: ${(tally / totalTally) * 100}%">
+    //                 ${tally}
+    //             </div>
+    //         </div>
+    //     </div>`;
 }
 
 const cards = document.getElementsByClassName('point-card');
@@ -109,6 +157,28 @@ for (const card of cards) {
 }
 
 document.getElementById('reveal').addEventListener('click', () => {
-
+    socket.emit("ROOM_UPDATE_REQUEST", roomId, 'reveal', (res) => {
+        renderRoom(res);
+    });
 });
 
+document.getElementById('reset').addEventListener('click', () => {
+    socket.emit("ROOM_UPDATE_REQUEST", roomId, 'hidden', (res) => {
+        renderRoom(res);
+    });
+});
+
+document.getElementById('change-name').addEventListener('click', () => {
+    playerName = document.getElementById('name-input').value;
+    socket.emit("PLAYER_UPDATE_REQUEST", roomId, playerId, { name: playerName }, (res) => {
+        document.getElementById('name-input').value = '';
+        sessionStorage.setItem('x-name', playerName);
+        renderRoom(res);
+    });
+});
+
+document.getElementById('copy-link').addEventListener('click', () => {
+    navigator.clipboard.writeText(location.href)
+    
+
+});
